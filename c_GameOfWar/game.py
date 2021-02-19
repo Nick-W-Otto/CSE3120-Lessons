9c4ac719050d4a2e7819f3ad4d28cc87962cdc6b
Nick Otto <n.otto@share.epsb.cagit config --global user.email n.otto@share.epsb.ca>
2021-02-19 14:22:32 -0700

    Began creating the game of war

---
+++ c_GameOfWar/game.py
@@ -0,0 +1,10 @@
+"""
+Title: Playing cards Game
+Author: Nick Otto
+Date: 2021. 02, 19
+"""
+
+class Game:
+    """
+
+    """
--- c_GameOfWar/player.py
+++ c_GameOfWar/player.py
@@ -4,8 +4,11 @@ Author: Nick Otto
 Date: 2021. 02, 18
 """
 
+# Two player classes with different arrays and pop values out of the deck into their hand using random pops
+
 from card import Card
 from deckpy import Deck
+from random import randrange
 
 class Player:
     """
@@ -13,14 +16,21 @@ class Player:
     """
 
     def __init__(self, NAME):
+
         self.NAME = NAME
         self.HAND = []
 
     # --- Modifier Methods --- #
+
     # Inputs #
 
 
+
     # Processing #
+
+
+
+
 
     # Outputs #
 
@@ -28,3 +38,18 @@ class Player:
 
     def getName(self):
         return self.NAME
+
+    def getDeck(self):
+        return self.HAND
+
+if __name__ == "__main__":
+    DECK = Deck()
+    p1Deck = []
+    p2Deck = []
+
+    splitDeck = []
+
+    splitDeck.append(DECK.pop(randrange(1, 26)))
+    for i in range(26):
+        p1Deck.append(DECK.pop(randrange(1, len(DECK))))
+    print(p1Deck)
